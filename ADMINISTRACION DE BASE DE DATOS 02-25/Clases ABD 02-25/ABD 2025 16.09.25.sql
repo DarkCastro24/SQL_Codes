33ed1fbeaa9e671df2c6891d4a18e1b073502974
DarkCastro24 <104780226+DarkCastro24@users.noreply.github.com>
2025-09-16 15:30:03 -0600

    add: abd class 16.09.25

---
+++ ADMINISTRACION DE BASE DE DATOS 02-25/Clases ABD 02-25/ABD 2025 16.09.25.sql
@@ -0,0 +1,48 @@
+-- Primera clase ABD del ing James								Fecha: 16/09/2025
+
+/*
+master:
+Es la base de datos más importante de SQL Server, ya que contiene toda la información a nivel de sistema como configuraciones del servidor, detalles de inicio de sesión, rutas de archivos físicos y referencias a todas las demás bases de datos. Si esta base se daña, 
+el motor de SQL Server no podrá arrancar correctamente.
+
+model:
+Sirve como plantilla para la creación de nuevas bases de datos; 
+todas las bases que se creen después de instalar SQL Server tomarán su configuración base desde model, incluyendo opciones como collation, tamaño inicial, y configuraciones de recuperación. No tiene efecto retroactivo sobre bases ya existentes.
+
+msdb:
+Almacena información relacionada con las tareas automatizadas del servidor, como trabajos programados, 
+planes de mantenimiento, alertas, operadores y el historial de respaldos y restauraciones. 
+Es fundamental para el funcionamiento del SQL Server Agent y otras operaciones administrativas.
+
+tempdb:
+Es una base de datos temporal que se reinicia cada vez que se reinicia el servidor; 
+se utiliza para almacenar objetos temporales como tablas temporales, variables tipo tabla, cursores, y 
+datos intermedios generados en operaciones de ordenamiento, agrupamiento o joins complejos. Su rendimiento es clave porque es ampliamente utilizada por SQL Server internamente.
+*/
+
+-- Consulta para obtener todas las bases de datos del SQL Server 
+SELECT name, database_id, state_desc, recovery_model_desc 
+FROM sys.databases
+WHERE database_id <= 4
+ORDER BY database_id
+
+-- Acceso a la tabla temporal
+SELECT TOP 20 OBJECT_NAME(object_id), *
+FROM tempdb.sys.objects
+ORDER BY create_date DESC;
+
+-- Listar bases de datos del sistema y su estado
+SELECT name, state_desc, recovery_model_desc
+FROM sys.databases
+WHERE database_id <= 4;
+
+-- Crear y comprobar una base de datos
+CREATE DATABASE LabIntroDBA;
+GO
+
+SELECT name, recovery_model_desc, collation_name
+FROM sys.databases
+WHERE name = 'LabIntroDBA';
+GO
+
+DROP DATABASE LabIntroDBA;
